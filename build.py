be3c5cc7d355b4a5c58848a85ac5da8dced19db1
Robin Leroy <egg.robin.leroy@gmail.com>
2025-08-09 20:48:49 +0200

    Clip 𒀱  if needed, don’t use it for the line spacing

--- build.py
+++ build.py
@@ -10,4 +10,14 @@ for glyph_name in font:
   font[glyph_name].right_side_bearing = int(font[glyph_name].right_side_bearing + 60)
 font.ascent = 800
 font.descent = 200
+font.os2_typoascent = 800
+font.os2_typoascent_add = False
+font.os2_typodescent = -200
+font.os2_typodescent_add = False
+font.os2_use_typo_metrics = True
+# Still better to clip than to space lines for 𒀱…
+font.os2_winascent = 900
+font.os2_winascent_add = False
+font.os2_windescent = 200
+font.os2_windescent_add = False
 font.generate("Nabuninuaihsus.otf")
